bd1da7407326c2c247d2593b5768f5c5ec31c7ab
Ifeel3 <xifeel3x@gmail.com>
2023-09-15 19:56:59 +0600

    complited?

--- shortener/src/main.go
+++ shortener/src/main.go
@@ -3,19 +3,64 @@ package main
 import (
 	"context"
 	"crypto/sha256"
+	"encoding/json"
 	"fmt"
+	"math"
 	"net/http"
 
 	"github.com/redis/go-redis/v9"
 )
 
-type Node struct {
-	url       string
-	redirects int
+type RedisWithCache struct {
+	cache     map[string]string
+	redirects map[string]uint
+	db        *redis.Client
 }
 
-func NewNode(url string) Node {
-	return Node{url, 0}
+func NewCache(db *redis.Client) RedisWithCache {
+	return RedisWithCache{make(map[string]string, 100), make(map[string]uint, 100), db}
+}
+
+func (rwc *RedisWithCache) deleteMinRedirects() {
+	var min uint = math.MaxUint
+	for _, val := range rwc.redirects {
+		if val < min {
+			min = val
+		}
+	}
+	for key, val := range rwc.redirects {
+		if val == min {
+			delete(rwc.cache, key)
+			delete(rwc.redirects, key)
+		}
+	}
+}
+
+func (rwc *RedisWithCache) Set(key string, value string) {
+	if key == "" || value == "" {
+		return
+	}
+	if len(rwc.redirects) > 100 {
+		rwc.deleteMinRedirects()
+	}
+	rwc.cache[key] = value
+	rwc.redirects[key] = 0
+	rwc.db.Set(context.TODO(), key, value, 0)
+}
+
+func (rwc *RedisWithCache) Get(key string) string {
+	res, ok := rwc.cache[key]
+	if !ok {
+		tmp, err := rwc.db.Get(context.TODO(), key).Result()
+		if err != nil {
+			return ""
+		}
+		rwc.cache[key] = tmp
+		rwc.redirects[key] = 0
+		res = tmp
+	}
+	rwc.redirects[key]++
+	return res
 }
 
 func Mapping(w http.ResponseWriter, r *http.Request) {
@@ -25,27 +70,30 @@ func Mapping(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Bad Request\n"))
 	} else {
 		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(url)))[:8]
-		db.Set(context.TODO(), hash, url, 0)
-		cache[hash] = NewNode(url)
+		cache.Set(hash, url)
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]interface{}{"hash": hash})
 	}
 }
 
 func Redirect(w http.ResponseWriter, r *http.Request) {
 	hash := r.RequestURI[3:len(r.RequestURI)]
-	w.Write([]byte(cache[hash].url))
-	cache[hash].redirects++
+	url := cache.Get(hash)
+	if url == "" {
+		w.Write([]byte("Not Found"))
+		return
+	}
+	http.Redirect(w, r, url, http.StatusMovedPermanently)
 }
 
-var db *redis.Client
-var cache map[string]Node
+var cache RedisWithCache
 
 func main() {
-	db = redis.NewClient(&redis.Options{
+	cache = NewCache(redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
 		DB:       0,
-	})
-	cache = make(map[string]Node, 100)
+	}))
 	http.HandleFunc("/a/", Mapping)
 	http.HandleFunc("/s/", Redirect)
 	http.ListenAndServe(":8080", nil)
